522b8d1aafb75dd8f35bb5de4ff51e1cb66eb020
Ziyao Zhang <ziyaointl@gmail.com>
2017-02-28 16:22:26 +0800

    Add basic html elements

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,31 @@
 <template>
     <div id="app">
+        <div class="container">
+            <div class="columns">
+                <div class="column is-6">
+                    <div class="green-button notification is-success">
 
+                    </div>
+                </div>
+                <div class="column is-6">
+                    <div class="red-button notification is-danger">
+
+                    </div>
+                </div>
+            </div>
+            <div class="columns">
+                <div class="column is-6">
+                    <div class="yellow-button notification is-warning">
+
+                    </div>
+                </div>
+                <div class="column is-6">
+                    <div class="blue-button notification is-info">
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -22,5 +47,7 @@
     @import "css/bulma/elements/_all.sass";
     @import "css/bulma/grid/_all.sass";
 
+    .green-button, .red-button {
     
+    }
 </style>
