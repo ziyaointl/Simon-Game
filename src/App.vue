ec94620bb5a0d79f93427fe46a84f7879c6275ae
Ziyao Zhang <ziyaointl@gmail.com>
2017-02-28 16:03:51 +0800

    Add and import Bulma

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,11 @@
 </script>
 
 <style lang="scss">
+    @import "css/bulma/utilities/_all.sass";
+    @import "css/bulma/base/_all.sass";
+    @import "css/bulma/layout/_all.sass";
+    @import "css/bulma/elements/_all.sass";
+    @import "css/bulma/grid/_all.sass";
 
     
 </style>
