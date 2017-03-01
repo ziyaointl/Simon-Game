3d7d07a5b71297d924cf5ff11a74180a1d24c3e9
Ziyao Zhang <ziyaointl@gmail.com>
2017-03-01 17:09:56 +0800

    Implement basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,38 @@
 <template>
     <div id="app">
-        <div class="container">
-            <div class="columns">
-                <div class="column is-6">
-                    <div class="green-button notification is-success">
+        <section class="hero is-fullheight is-bold is-dark">
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <div class="columns">
+                        <div class="column is-3 is-offset-3">
+                            <div class="green-button notification is-success">
 
-                    </div>
-                </div>
-                <div class="column is-6">
-                    <div class="red-button notification is-danger">
+                            </div>
+                        </div>
+                        <div class="column is-3">
+                            <div class="red-button notification is-danger">
 
+                            </div>
+                        </div>
                     </div>
+                    <div class="columns">
+                        <div class="column is-3 is-offset-3">
+                            <div class="yellow-button notification is-warning">
+
+                            </div>
+                        </div>
+                        <div class="column is-3">
+                            <div class="blue-button notification is-info">
+
+                            </div>
+                        </div>
+                    </div>
+                    <a class="button is-dark is-inverted is-outlined has-text-centered is-large">Start</a>
+                    <a class="button is-dark is-inverted is-outlined has-text-centered is-large">Help</a>
+                    <a class="button is-dark is-inverted is-outlined has-text-centered is-large">Score: {{score}}</a>
                 </div>
             </div>
-            <div class="columns">
-                <div class="column is-6">
-                    <div class="yellow-button notification is-warning">
-
-                    </div>
-                </div>
-                <div class="column is-6">
-                    <div class="blue-button notification is-info">
-
-                    </div>
-                </div>
-            </div>
-        </div>
+        </section>
     </div>
 </template>
 
@@ -34,7 +41,7 @@
         name: 'app',
         data () {
             return {
-
+                score: 0
             }
         }
     }
@@ -47,7 +54,8 @@
     @import "css/bulma/elements/_all.sass";
     @import "css/bulma/grid/_all.sass";
 
-    .green-button, .red-button {
-    
+    .green-button, .red-button, .yellow-button, .blue-button {
+        height: 30vh;
     }
+
 </style>
